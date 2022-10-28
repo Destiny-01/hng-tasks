d511a31895057dcc35b6c0025193092ef0ac268b
Destiny Aigbe <aigbedestinyic@gmail.com>
2022-10-28 21:09:07 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const sever = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  //   res.writeHead(200);
+  res.end(
+    JSON.stringify({
+      slackUsername: "diac",
+      backend: true,
+      age: 12,
+      bio: "This should be a description of me... I guess",
+    })
+  );
+});
+
+sever.listen(8000);
